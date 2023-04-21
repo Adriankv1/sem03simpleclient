69a9db6f0fc1ac7141940f62c17122a1db0a4da4
Adriankv1 <Adriankv@uia.no>
2023-04-21 16:45:34 +0200

    fix

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Adriankv1/sem03simpleclient
+
+go 1.20
+
+require github.com/Adriankv1/is105sem03 v0.0.0-20230417093202-03158c35e687
--- tcp_client.go
+++ tcp_client.go
@@ -1,42 +1,34 @@
 package main
 
 import (
-        "log"
-        "net"
-        "os"
+	"log"
+	"net"
+	"os"
 
-        "github.com/Adriankv1/is105sem03/mycrypt"
+	"github.com/Adriankv1/is105sem03/mycrypt"
 )
 
 func main() {
-        conn, err := net.Dial("tcp", "172.17.0.3:33419")
-        if err != nil {
-                log.Fatal(err)
-        }
+	conn, err := net.Dial("tcp", "172.17.0.3:33419")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALFSEM03, 4)
-        log.Println("Kryptert melding: ", string(kryptertMelding))
-        , err = conn.Write([]byte(string(kryptertMelding)))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 
-        log.Println("os.Args[1] = ", os.Args[1])
+	log.Println("Kryptert melding: ", string(kryptertMelding))
 
-        _, err = conn.Write([]byte(os.Args[1]))
-        if err != nil {
-                log.Fatal(err)
-        }")
-        if err != nil {
-                log.Fatal(err)
-        }
+	_, err = conn.Write([]byte(string(kryptertMelding)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALFSEM03, 4)
-        log.Println("Kryptert melding: ", string(kryptertMelding))
-        , err = conn.Write([]byte(string(kryptertMelding)))
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, (len(mycrypt.ALF_SEM03) - 4))
+	log.Printf("relpy from proxy: %s", string(dekryptertMelding))
 
-        log.Println("os.Args[1] = ", os.Args[1])
-
-        _, err = conn.Write([]byte(os.Args[1]))
-        if err != nil {
-                log.Fatal(err)
-        }
 }
-
